2690b40b12b855bbf4c2c82bf1a30cbdef47d148
glpat-JeRBose9xiehRzy1yF6P <patelkashif08@gmail.com>
2025-01-04 13:17:48 +0530

    cc_weather task

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e241f6f3e6182acbfcb0588034e8fb43",
+  base: "http://api.weatherstack.com/current",
+};
+
